469a23403f8ddee9e64a35d97f855b22dea14cc0
nullyng <young315@knu.ac.kr>
2021-08-29 18:05:01 +0900

    Update LoginPage

--- src/components/views/Pages/LoginPage.js
+++ src/components/views/Pages/LoginPage.js
@@ -1,8 +1,33 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+
 import './styles.css';
+import { withStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        margin: theme.spacing(1),
+        width: '30ch',
+    },
+}))
+
+const CssTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: 'green',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'green',
+      },
+    },
+  })(TextField);
 
 function LoginPage(props) {
+    const classes = useStyles();
     const [Id, setId] = useState("");
     const [Password, setPassword] = useState("");
 
@@ -30,13 +55,25 @@ function LoginPage(props) {
         <div className="login-background">
             <div className="login">
             <img alt="logo" src="https://image.flaticon.com/icons/png/512/1598/1598431.png"/>
-            <form onSubmit={onSubmitHandler}>
+            <form className={classes.root} noValidate autoComplete="off" onSubmit={onSubmitHandler}>
+                관리자페이지
                 <h1>로그인</h1>
-                <input placeholder={'아이디를 입력하세요.'} value={Id} onChange={onIdHandler} />
-                <br />
-                <input type="password" placeholder={'비밀번호를 입력하세요.'} value={Password} onChange={onPasswordHandler} />
-
-                <br/>
+                <CssTextField
+                label="아이디"
+                className="textfield-id"
+                name="아이디"
+                onChange={onIdHandler}
+                type="text"
+                fullWidth
+                margin="normal" />
+                <CssTextField
+                label="비밀번호"
+                className="textfield-password"
+                name="비밀번호"
+                onChange={onPasswordHandler}
+                type="password"
+                fullWidth
+                margin="normal" />
                 <button className="login-button">
                     로그인
                 </button>
