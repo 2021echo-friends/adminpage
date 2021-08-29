a23d25759fac0b87a2da93b6937d57425238d982
nullyng <young315@knu.ac.kr>
2021-08-29 15:56:30 +0900

    Add body-parser

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use('/', require('./routes/index'));
+
+// application/x-www-form-urlencoded 데이터를 분석해서 가져올 수  있게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 타입으로 된 것을 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const port = 5000;
 app.listen(port, () => {
